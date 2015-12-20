d38d84b97fbcc6417189f7917214b3cd630d88bd
Kate <katy-lyubkevic@yandex.ru>
2015-12-20 16:14:07 +0200

    1. Создание проекта и первого теста. Singleton создает лишь один обьект.

---
+++ CW/MyTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTests
+{
+    [TestClass]
+    public class HumanTest
+    {
+        public void CheckTypeSingleton()
+        {
+
+            Singleton w = Singleton.GetInstance();
+            Singleton m = Singleton.GetInstance();
+
+            Assert.AreSame(m, w);
+        }
+    }
+}
