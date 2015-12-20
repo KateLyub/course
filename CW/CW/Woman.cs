df4529bfcbed9e07060c1d2ab3345f3d76456ee3
Kate <katy-lyubkevic@yandex.ru>
2015-12-20 17:07:34 +0200

    10. Добавляем действия женщинам.

--- CW/CW/Woman.cs
+++ CW/CW/Woman.cs
@@ -9,5 +9,20 @@ namespace CW
     public class Woman
     {
         public Woman() : base() { }
+
+        public void WomanWalking()
+        {
+            Console.WriteLine("Ходит как женщина.");
+        }
+
+        public void WomanEating()
+        {
+            Console.WriteLine("Ест как женщина.");
+        }
+
+        public void WomanDressing()
+        {
+            Console.WriteLine("Одевается как женщина.");
+        }
     }
 }
