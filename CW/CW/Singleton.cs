ff5cde3d32bfd1a69459438d1315814daf303dd1
Kate <katy-lyubkevic@yandex.ru>
2015-12-20 16:21:25 +0200

    2. Ленивая инициализация одиночки.

---
+++ CW/CW/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        public static Singleton Instance
+        {
+            get { return instance ?? (instance = new Singleton()); }
+        }
+        protected Singleton() { }
+    }
+}
